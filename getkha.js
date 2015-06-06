22b304c261b1593bf8b6d7053416a33cdd88b6ea
Robert Konrad <robert@ktx-software.com>
2015-06-06 15:49:14 +0200

    Changed getkha script.

--- getkha.js
+++ getkha.js
@@ -1,8 +1,1 @@
-var version = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
-
-if (version < 0.12) {
-	console.log('Sorry, this requires at least node version 0.12.')
-	process.exit(1);
-}
-
-require('child_process').execFileSync('git', ['clone', '--recursive', 'https://github.com/ktxsoftware/Kha.git']);
+require('child_process').spawn('git', ['clone', '--recursive', 'https://github.com/ktxsoftware/Kha.git'], { stdio: 'inherit' });
