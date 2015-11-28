2932ffc383589072b36cf41a5ca91ec24e9b8703
Robert Konrad <robert@ktx-software.com>
2015-11-28 20:22:46 +0100

    Kha update.

--- khafile.js
+++ khafile.js
@@ -1,11 +1,7 @@
 var project = new Project('Blocks');
 
-project.addLibrary('Kha2D');
-
 project.addAssets('Assets/**');
 
 project.addSources('Sources');
-
-project.addShaders('Sources/Shaders');
 
 return project;
